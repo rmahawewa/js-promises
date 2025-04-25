62ba83dc6c067368b3ff9250bcf0bfb7343ee29f
rmahawewa <rmahawewa@yahoo.com>
2025-04-25 12:39:53 +0530

    practice Promise.all api

---
+++ promise_api_s.js
@@ -0,0 +1,41 @@
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("p1 success"), 3000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("p2 success"), 1000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("p3 success"), 6000);
+});
+
+const p4 = new Promise((resolve, reject) => {
+    setTimeout(() => reject("p4 fail"), 1000);
+});
+
+const p5 = new Promise((resolve, reject) => {
+    setTimeout(() => reject("p5 fail"), 4000);
+});
+
+const p6 = new Promise((resolve, reject) => {
+    setTimeout(() => reject("p6 fail"), 5000);
+});
+
+Promise.all([p1, p2, p3])
+.then((res) => {
+    console.log("1", res);
+})
+.catch((err) => {
+    console.error(err);
+    console.log(err.errors);
+});
+
+Promise.all([p1,p2,p4])
+.then((res) => {
+    console.log("2", res);
+})
+.catch((err) => {
+    console.error("2", err);
+    console.log("2", err.errors);
+});
